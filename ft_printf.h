8b3032ddd242fc58da0dadd956feaaa9e2975f93
Winter <matwinte.42@bluewin.ch>
2022-11-20 02:02:03 +0100

    proto header

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,15 @@
 /*   By: mwinter <mwinter@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 19:28:40 by mwinter           #+#    #+#             */
-/*   Updated: 2022/11/19 19:28:41 by mwinter          ###   ########.fr       */
+/*   Updated: 2022/11/20 00:54:04 by mwinter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include "./libft/libft.h"
+# include <stdarg.h>
+
+int	ft_printf(const char *format, ...);
+
+#endif
